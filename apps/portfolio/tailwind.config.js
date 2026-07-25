dc6cdb8b60250aef4893ef0beb4d3cb285c075eb
Luann Moreira <luannmcurioso@gmail.com>
2026-07-25 16:16:13 -0400

    build: extract packages/config-tailwind

--- apps/portfolio/tailwind.config.js
+++ apps/portfolio/tailwind.config.js
@@ -1,25 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  presets: [require("@portfolio/config-tailwind")],
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        dark: {
-          50: "#434343",
-          100: "#0f172a",
-          200: "#0f172a",
-          300: "#252525",
-          400: "#1b1b1b",
-          500: "#0A0D11",
-          600: "#070707",
-          700: "#000000",
-          800: "#000000",
-          900: "#000000",
-        },
-        white: "#ffffff",
-        blue: "#14B8A6",
-      },
-    },
-  },
   plugins: [],
 };
